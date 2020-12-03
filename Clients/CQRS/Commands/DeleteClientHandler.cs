778dd55916a3c19a9e5a6fcb7dcbd8c43e3bc0a7
josh seward <Josh.seward92@gmail.com>
2020-12-03 21:03:40 +0000

    Added in the update and the delete

--- Clients/CQRS/Commands/DeleteClientHandler.cs
+++ Clients/CQRS/Commands/DeleteClientHandler.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using Clients.Database.Entities;
+using Clients.Database.Repositories;
 using MediatR;
 
 namespace Clients.CQRS.Commands
@@ -19,8 +21,27 @@ namespace Clients.CQRS.Commands
 
 	public class DeleteClientHandler : IRequestHandler<DeleteClientRequest>
 	{
+		private readonly IRepository<Client> _clientRepository;
+
+		public DeleteClientHandler(IRepository<Client> clientRepository)
+		{
+			_clientRepository = clientRepository;
+		}
+
 		public async Task<Unit> Handle(DeleteClientRequest request, CancellationToken cancellationToken)
 		{
+			if (request == null)
+			{
+				throw new ArgumentException("Request cannot  be null");
+			}
+
+			var client = await _clientRepository.Get(request.ClientId);
+
+			if(client != null)
+			{
+				_clientRepository.Delete(client);
+				await _clientRepository.SaveChangesAsync();
+			}			
 			return Unit.Value;
 		}
 	}
--- Clients/CQRS/Commands/UpdateClientHandler.cs
+++ Clients/CQRS/Commands/UpdateClientHandler.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading;
 using System.Threading.Tasks;
+using AutoMapper;
+using Clients.Database.Entities;
+using Clients.Database.Repositories;
 using Clients.Dtos;
 using MediatR;
 
@@ -20,8 +23,29 @@ namespace Clients.CQRS.Commands
 
 	public class UpdateClientHandler : IRequestHandler<UpdateClientRequest>
 	{
+		private readonly IRepository<Client> _clientRepository;
+		private readonly IMapper _mapper;
+
+		public UpdateClientHandler(IRepository<Client> clientRepository, IMapper mapper)
+		{
+			_clientRepository = clientRepository;
+			_mapper = mapper;
+		}
+
 		public async Task<Unit> Handle(UpdateClientRequest request, CancellationToken cancellationToken)
 		{
+			if (request == null)
+			{
+				throw new ArgumentException("Request cannot  be null");
+			}
+
+			var client = await _clientRepository.Get(request.Client.ClientId);
+
+			_mapper.Map(request.Client, client);
+
+			_clientRepository.Update(client);
+			await _clientRepository.SaveChangesAsync();
+
 			return Unit.Value;
 		}
 	}
